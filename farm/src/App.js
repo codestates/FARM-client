a1609027552b78e9b10f14e376d4966ddfb3eb79
youngyounkim <rladuddbs789@gmail.com>
2021-04-29 10:19:04 +0900

    [FarmPage] 라우터 수정 App 컴포넌트에서 잘못 호출되고 있던 FarmPage 컴포넌트 호출 방식 수정

--- farm/src/App.js
+++ farm/src/App.js
@@ -4,18 +4,17 @@ import FarmPage from "./Pages/FarmPage";
 
 function App() {
   return (
-    <FarmPage></FarmPage>
-    // <Router>
-    //   <Switch>
-    //     <Route path="/">{/* hello world 찍어주는 컴포넌트 */}</Route>
-    //     <Route path="/signin">{/* 로그인 컴포넌트 */}</Route>
-    //     <Route path="/signup">{/* 회원가입 컴포넌트 */}</Route>
-    //     <Route path="/mypage">{/* 마이페이지 컴포넌트 */}</Route>
-    //     <Route path="/farmpage">
-    //       <FarmPage></FarmPage>
-    //     </Route>
-    //   </Switch>
-    // </Router>
+    <Router>
+      <Switch>
+        <Route path="/">
+          <FarmPage></FarmPage>
+        </Route>
+        <Route path="/signin">{/* 로그인 컴포넌트 */}</Route>
+        <Route path="/signup">{/* 회원가입 컴포넌트 */}</Route>
+        <Route path="/mypage">{/* 마이페이지 컴포넌트 */}</Route>
+        <Route path="/farmpage"></Route>
+      </Switch>
+    </Router>
   );
 }
 
