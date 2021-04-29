f07ee11ed3f2cf5f88aeafbad4e7fb3c03c2e1e8
rjc1704 <rjc1704@gmail.com>
2021-04-29 16:42:09 +0900

    [FarmPage][ReadFarmSeed] actions.js  괄호 오류 수정

--- farm/src/Redux/actions/actions.js
+++ farm/src/Redux/actions/actions.js
@@ -4,7 +4,6 @@ export const DELETE_SEED = "DELETE_SEED";
 
 export const ADD_SEEDS = "ADD_SEEDS";
 
-
 export const addCrops = (name, icon, idx) => {
   return {
     type: ADD_CROPS,
@@ -16,21 +15,21 @@ export const addCrops = (name, icon, idx) => {
   };
 };
 
-
 export const deleteSeed = (farmerId, seedId) => {
   return {
     type: DELETE_SEED,
     payload: {
       farmerId: farmerId,
       seedId: seedId,
-
+    },
+  };
+};
 export const addSeeds = (id, name) => {
   return {
     type: ADD_SEEDS,
     payload: {
       id,
       name,
-
     },
   };
 };
