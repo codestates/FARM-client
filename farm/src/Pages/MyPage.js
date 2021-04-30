644058e2dfea87ff0091a8eaac7c46828637f566
youngyounkim <rladuddbs789@gmail.com>
2021-04-30 12:18:30 +0900

    [MyPage][ReadFarmList]화면에 farmlist를 출력하는 기능 추가 리덕스 store에 있는 farmlist를 호출하여 출력하는 기능 추가

--- farm/src/Pages/MyPage.js
+++ farm/src/Pages/MyPage.js
@@ -1,9 +1,11 @@
 import ReadFarmerInfo from "../Components/ReadFarmerInfo";
+import ReadFarmList from "../Components/ReadFarmList";
 
 export default function myPage() {
   return (
     <div>
       <ReadFarmerInfo />
+      <ReadFarmList />
     </div>
   );
 }
