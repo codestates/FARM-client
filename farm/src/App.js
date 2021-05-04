52065c876075ba9da03016cfd79c205c6731a165
rjc1704 <rjc1704@gmail.com>
2021-05-04 23:20:17 +0900

    [SignInPage] 로그인 회원가입 로직 다듬기 1. 회원가입 시 이미 가입한 이메일 입력하면 존재하는 회원이라고 메시지 띄우기 2. 에러 메시지 붉은 글씨 아닌 부분 스타일 입히기

--- farm/src/App.js
+++ farm/src/App.js
@@ -41,11 +41,10 @@ function App() {
       .then((res) => {
         if (res.data.message !== "ok") {
           alert("로그인 기간이 만료되었습니다. 다시 로그인 해주세요.");
-          // setIslogin(false);
           dispatch(setNoAuth());
           return history.push("/");
         }
-        // userinfo를 잘 불러 왔으면 dis
+
         const objUserInfo = res.data.data.userinfo;
         axios
           .get(`${url}/users/farminfo`, {
@@ -58,16 +57,14 @@ function App() {
           .then((res) => {
             if (res.data.message !== "ok") {
               alert("로그인 기간이 만료되었습니다. 다시 로그인 해주세요.");
-              // setIslogin(false);
               dispatch(setNoAuth());
               return history.push("/");
             }
             const arrProjectList = res.data.data;
             dispatch(setUserInfo(objUserInfo, arrProjectList));
-            // return history.push("/");
           });
       })
-      .catch((err) => alert(err));
+      .catch((err) => console.log(`err`, err));
   };
   const handleLogout = () => {
     //로그아웃 요청
