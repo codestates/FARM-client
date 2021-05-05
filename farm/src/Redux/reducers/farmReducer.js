447d7ec29ab6f4879a0560c9532f32087a2470ba
DaeseongKim <eotjd1105@icloud.com>
2021-05-06 00:22:01 +0900

    [FarmPage][ReadSeeds] Seed 삭제 기능 추가. 1. Components/DeleteSeed.js 추가 2. DELETE_SEED_FROM_CROP Action 추가 3. farmReducer case 추가

--- farm/src/Redux/reducers/farmReducer.js
+++ farm/src/Redux/reducers/farmReducer.js
@@ -8,6 +8,7 @@ import {
   SET_FARM,
   INVITE_FARMERS,
   SET_STORAGE,
+  DELETE_SEED_FROM_CROP,
 } from "../actions/actions";
 
 const farmReducer = (state = dummy, action) => {
@@ -158,6 +159,27 @@ const farmReducer = (state = dummy, action) => {
     case SET_STORAGE:
       return Object.assign({}, state, {
         storage: action.payload.arrStorage,
+      });
+
+    case DELETE_SEED_FROM_CROP:
+      // let cropArr = state.crops.filter(
+      //   (crop) => (crop.crops_id === action.payload.cropsId)
+      // );
+
+      return Object.assign({}, state, {
+        // cropArr.Seeds.filter(el => el.seed_id !== action.payload.seedId )
+        crops: state.crops.map((crop) => {
+          if (crop.crops_id === action.payload.cropsId) {
+            return {
+              ...crop,
+              Seeds: crop.Seeds.filter((seed) => {
+                return seed.seed_id !== action.payload.seedId;
+              }),
+            };
+          } else {
+            return crop;
+          }
+        }),
       });
 
     default:
