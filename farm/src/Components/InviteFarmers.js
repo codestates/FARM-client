081dea8358b58bf40cde95d8482427200fd0f1f4
youngyounkim <rladuddbs789@gmail.com>
2021-05-05 15:39:07 +0900

    [FarmPage][InviteFarmers] 공통 modal 창이 출력되는 기능으로 변경

--- farm/src/Components/InviteFarmers.js
+++ farm/src/Components/InviteFarmers.js
@@ -4,18 +4,19 @@ import { useSelector, useDispatch } from "react-redux";
 // import invite from "../../../../FARM-server/controllers/farm/invite";
 import axios from "axios";
 import { inviteFarmers } from "../Redux/actions/actions";
+import Modal from "./Modal";
 
 export default function InviteFarmers() {
   const emailInput = useRef();
-  const [isOpenModal, setIsOpenModal] = useState(false);
+  const [isModal, setIsModal] = useState(false);
   const [strEmail, setStrEmail] = useState("");
+  const [strErr, setStrErr] = useState("");
   const authState = useSelector((state) => state.authReducer);
   const state = useSelector((state) => {
     return state.farmReducer;
   });
   const dispatch = useDispatch();
-  const inviteFarmer = async (e) => {
-    e.preventDefault();
+  const inviteFarmer = async () => {
     // 서버에 strEmail 담아서 post 요청. 요청 정보 없으면 modal창에 안내문구 추가. 있으면 추가처리하기 모달창 닫기
     try {
       const objRes = await axios.post(
@@ -35,35 +36,52 @@ export default function InviteFarmers() {
       if (objRes.data.message === "new member added") {
         dispatch(inviteFarmers(objRes.data.id, objRes.data.username, strEmail));
       }
-      handleModal();
+      setIsModal(false);
     } catch (err) {
-      alert("이미 초대된 농부이거나 존재하지 않는 농부입니다.");
+      setStrErr("이미 초대된 농부이거나 존재하지 않는 농부입니다.");
     }
-  };
-  const handleModal = () => {
-    setIsOpenModal(!isOpenModal);
   };
 
   const handleEmail = (e) => {
     setStrEmail(e.target.value);
   };
+
+  const openModal = () => {
+    setIsModal(true);
+  };
+  const closeModal = () => {
+    setIsModal(false);
+    setStrErr("");
+  };
   return (
     <div className="Farmer_Wrapper">
       <div
-        className={isOpenModal ? "Modal_Background" : "Farmer_Header"}
-        onClick={handleModal}
+        className={isModal ? "Modal_Background" : "Farmer_Header"}
+        onClick={openModal}
       >
         새로운 농부 초대하기
-        {isOpenModal ? (
-          <form
-            onSubmit={inviteFarmer}
-            className="Invite_Modal"
-            onClick={(e) => e.stopPropagation()}
+        {isModal ? (
+          <Modal
+            open={openModal}
+            close={closeModal}
+            header="새로운 농부 초대하기"
+            btntext="초 대"
+            callback={inviteFarmer}
           >
-            초대할 농부의 이메일을 입력하세요
-            <input type="text" ref={emailInput} onChange={handleEmail} />
-            <input type="submit" value="초 대" className="Invite_Btn" />
-          </form>
+            <form
+              onSubmit={(e) => {
+                e.preventDefault();
+                inviteFarmer();
+              }}
+              className="Invite_Farmer_Form"
+            >
+              <p style={{ color: "black" }}>
+                초대할 농부의 이메일을 입력하세요
+              </p>
+              <input type="text" ref={emailInput} onChange={handleEmail} />
+              <p style={{ color: "red" }}>{strErr}</p>
+            </form>
+          </Modal>
         ) : null}
       </div>{" "}
     </div>
